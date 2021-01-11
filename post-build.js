3572ed2e2937a092b4e8ce328c770ec3c84bb8d0
soulteary <soulteary@gmail.com>
2021-01-11 19:48:19 +0800

    add es5, minify build

--- post-build.js
+++ post-build.js
@@ -14,4 +14,13 @@ let content = raw
   // #fix warning at #L1731
   .replace(/\}\s+return true;\n}};/, '\}\n}};');
 
+// #fix for njs
+const repairFragment = `var lastColumn = ''; if (lines && lines.length) { lastColumn = lines[lines.length - 1].length - lines[lines.length - 1].match(/\\r?\\n?/)[0].length; } else { lastColumn = this.yylloc.last_column + match[0].length; } this.yylloc = { first_line: this.yylloc.last_line, last_line: this.yylineno + 1, first_column: this.yylloc.last_column, last_column: lastColumn };`
+const needRepairs = content.match(/this\.yylloc = \{\n[\s\S]+?\};/gm);
+if(needRepairs.length){
+  needRepairs.filter(text => text.includes('this.yylloc.last_column')).forEach(function(text){
+    content = content.replace(text, repairFragment);
+  });
+}
+
 writeFileSync("./dist/nomnoml.es5.js", content);
